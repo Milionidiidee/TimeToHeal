c933996906daf7ffc30d1c743db5afe963ce3a6a
Tommaso Giubilei <tms.gbl@gmail.com>
2022-07-05 16:22:10 +0200

    Configuring Input System (not finished, cannot aim with gamepad))
    - Install of the New Unity Input System
    - Configuring Inputs
    (GamePad Aim is not yet supported)

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PlayerInputs _input;
+
+    //Aim and Fire Variables
+    Vector3 _aimPoint;
+    float _requestFire = 0;                                        //It's <0.5 if we don't want to fire
+
+    //Move Variables
+    Vector2 _desiredMoveDirection = Vector2.zero;
+
+    void Awake()
+    {
+        _input = new PlayerInputs();
+
+        _input.PlayerControls.Run.performed += ctx => _desiredMoveDirection = ctx.ReadValue<Vector2>();
+        _input.PlayerControls.Fire.performed += ctx => _requestFire = ctx.ReadValue<float>();
+        _input.PlayerControls.Aim.performed += ctx => {
+            //Assuming the input comes from the mouse position
+            Vector2 screenPoint = ctx.ReadValue<Vector2>();
+            _aimPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenPoint.x, screenPoint.y, Camera.main.nearClipPlane));
+        };
+
+    }
+
+    void OnEnable()
+    {
+        _input.PlayerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        _input.PlayerControls.Disable();
+    }
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
+    {
+        Debug.Log(_requestFire);
+    }
+
+    void FixedUpdate()
     {
         
     }
