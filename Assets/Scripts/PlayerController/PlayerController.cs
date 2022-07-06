222e472d746360385f21277e13440a2e48dd0269
Tommaso Giubilei <tms.gbl@gmail.com>
2022-07-07 00:15:42 +0200

    Input System
    Gamepad support is not tested and completed but keyboard and mouse works fine

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,21 +13,39 @@ public class PlayerController : MonoBehaviour
 
     //Aim and Fire Variables
     Vector3 _aimPoint;
-    float _requestFire = 0;                                        //It's <0.5 if we don't want to fire
+    float _requestFire = 0;                                         //It's <0.5 if we don't want to fire
+
+    [Range(5f,20f)]
+    [SerializeField] [Tooltip("Maximum distance a player can aim to")]
+    float _maxGamepadAimDistance = 20f;
 
     //Move Variables
     Vector2 _desiredMoveDirection = Vector2.zero;
 
     void Awake()
     {
+        //Input Management System
+        //When an event is triggered the input is set in the correct variables to be then executed
         _input = new PlayerInputs();
 
-        _input.PlayerControls.Run.performed += ctx => _desiredMoveDirection = ctx.ReadValue<Vector2>();
+        _input.PlayerControls.Run.performed += ctx => _desiredMoveDirection = ctx.ReadValue<Vector2>().normalized;
         _input.PlayerControls.Fire.performed += ctx => _requestFire = ctx.ReadValue<float>();
         _input.PlayerControls.Aim.performed += ctx => {
-            //Assuming the input comes from the mouse position
-            Vector2 screenPoint = ctx.ReadValue<Vector2>();
-            _aimPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenPoint.x, screenPoint.y, Camera.main.nearClipPlane));
+            if (ctx.control.device.name == "Mouse")
+            {
+                //Assuming the input comes from the mouse position
+                Vector2 screenPoint = ctx.ReadValue<Vector2>();
+                _aimPoint = Camera.main.ScreenToWorldPoint(new Vector3(screenPoint.x, screenPoint.y, Camera.main.nearClipPlane));
+                _aimPoint.z = 0;
+            }
+            else
+            {
+                //The only other supported device currently is a GamePad
+                Vector2 aimPos = ctx.ReadValue<Vector2>();
+                _aimPoint = new Vector3(transform.position.x + aimPos.x * _maxGamepadAimDistance,
+                                            transform.position.y + aimPos.y * _maxGamepadAimDistance,
+                                            0);
+            }
         };
 
     }
@@ -45,11 +67,26 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        Debug.Log(_requestFire);
+        
     }
 
     void FixedUpdate()
     {
         
     }
+
+#if UNITY_EDITOR
+    //Code to make the editor more intuitive for the level designer
+    void OnDrawGizmosSelected()
+    { 
+        Gizmos.color = Color.green;
+        Handles.color = Color.green;
+
+        Handles.DrawWireDisc(transform.position, transform.forward, _maxGamepadAimDistance);
+        Handles.DrawSolidDisc(_aimPoint, transform.forward, .1f);
+
+        Handles.color = Color.white;
+        Gizmos.color = Color.white;
+    }
+#endif
 }
